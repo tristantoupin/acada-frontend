45d7ce8d4e3b2392b29c30e362fd448658693bd9
tristantoupin <tristantoupin@gmail.com>
2024-02-26 14:02:50 -0500

    added button functionality

--- src/components/home/IntroCTA/index.tsx
+++ src/components/home/IntroCTA/index.tsx
@@ -1,6 +1,7 @@
 import { MacbookScroll } from "components/ui/macbook-scroll";
 import Preview from "assets/HomePreview.png";
-import MagicBorderButton from "components/buttons/MagicBorderButton";
+import { Button } from "components/ui/button";
+import { IconBrandGoogle } from "@tabler/icons-react";
 
 export const IntroCTA = () => {
     return (
@@ -19,15 +20,14 @@ export const IntroCTA = () => {
 const Content = () => {
     return (
         <div className="space-y-4 w-full">
-            <div className="space-x-4">
-                <MagicBorderButton
-                    text="Sign up with Email"
-                    onClick={() => {}}
-                />
-                <MagicBorderButton
-                    text="Sign up with Google"
-                    onClick={() => {}}
-                />
+            <div className="space-x-4 flex justify-center">
+                <Button variant="magic" size="lg">
+                    Sign up with Email
+                </Button>
+                <Button variant="magic" size="lg" className="bg-blue-500 hover:bg-blue-500/90 items-center space-x-2">
+                    <IconBrandGoogle className="h-full"/>
+                    <span>Sign up with Google</span>
+                </Button>
             </div>
             <div className="w-full text-center">
                 Acada is an AI-base tutor that helps you learn your subjects
